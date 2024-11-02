a714080e48a4a4f2937d954eaa6afae786bfc61f
EmrePW <emreeyahya@gmail.com>
2024-11-02 16:46:10 +0300

    implement dropdown

--- src/app/country-list/country-list.component.ts
+++ src/app/country-list/country-list.component.ts
@@ -14,14 +14,33 @@ import { RouterModule } from '@angular/router';
 export class CountryListComponent {
   countries: Country[] | undefined = [];
   @ViewChild('search_box') input: ElementRef | undefined;
+  @ViewChild('dropdown') dropdown: ElementRef | undefined;
   constructor(private countryService: CountryService) {
     this.countries = this.countryService.countries.value ?? [];
   }
   searchCountry() {
-    let search_term = this.input?.nativeElement.value ?? '';
+    let search_term: string = this.input?.nativeElement.value ?? '';
     let filteredCountries: Country[] | undefined = [];
     filteredCountries = this.countryService.countries.value?.filter((prd) =>
-      prd.name.includes(search_term)
+      prd.name.toLowerCase().includes(search_term.toLowerCase())
+    );
+    this.countries = filteredCountries;
+  }
+  showDropdown() {
+    const arrow = document.querySelector('.fa-angle-down');
+    const menu = document.querySelector('.dropdown-menu');
+    if (menu?.classList.contains('hidden')) {
+      arrow?.classList.add('dropdown-active');
+      menu?.classList.remove('hidden');
+      return;
+    }
+    arrow?.classList.remove('dropdown-active');
+    menu?.classList.add('hidden');
+  }
+  dropdownFilter(region: string) {
+    let filteredCountries: Country[] | undefined = [];
+    filteredCountries = this.countryService.countries.value?.filter(
+      (prd) => prd.region == region
     );
     this.countries = filteredCountries;
   }
