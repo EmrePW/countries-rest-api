8548f7b7a067449184a248c515380b4f059b57b1
EmrePW <emreeyahya@gmail.com>
2024-11-02 15:52:24 +0300

    implement search feature

--- src/app/country-list/country-list.component.ts
+++ src/app/country-list/country-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { CountryService } from '../country.service';
 import { Country } from '../country';
 import { CommonModule } from '@angular/common';
@@ -12,8 +12,17 @@ import { RouterModule } from '@angular/router';
   styleUrl: './country-list.component.scss',
 })
 export class CountryListComponent {
-  countries: Country[] = [];
+  countries: Country[] | undefined = [];
+  @ViewChild('search_box') input: ElementRef | undefined;
   constructor(private countryService: CountryService) {
     this.countries = this.countryService.countries.value ?? [];
+  }
+  searchCountry() {
+    let search_term = this.input?.nativeElement.value ?? '';
+    let filteredCountries: Country[] | undefined = [];
+    filteredCountries = this.countryService.countries.value?.filter((prd) =>
+      prd.name.includes(search_term)
+    );
+    this.countries = filteredCountries;
   }
 }
